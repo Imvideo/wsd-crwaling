8142081de1167b50a300614c10c6d47660be34cd
Jeon Young Sang <iwtl0303@naver.com>
2024-12-04 08:57:44 +0900

    feat: init

---
+++ src/main/java/com/example/wsd_crawling/WsdCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.wsd_crawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdCrawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wsd_crawling/WsdCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsd_crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
