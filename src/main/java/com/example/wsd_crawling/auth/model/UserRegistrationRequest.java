11ad4255e7aa4bc4df5b0dffe971f1adb68d158f
Jeon Young Sang <iwtl0303@naver.com>
2024-12-07 20:36:15 +0900

    feat: UserRegistrationRequest 추가

---
+++ src/main/java/com/example/wsd_crawling/auth/model/UserRegistrationRequest.java
@@ -0,0 +1,10 @@
+package com.example.wsd_crawling.auth.model;
+
+public class UserRegistrationRequest {
+
+    private String email;
+    private String password;
+    private String username;
+
+    // Getter, Setter, Constructor
+}
