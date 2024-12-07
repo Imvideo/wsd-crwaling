66466247df027ba16674f1bf3d92329c8840d647
Jeon Young Sang <iwtl0303@naver.com>
2024-12-07 20:35:41 +0900

    feat: User 추가

---
+++ src/main/java/com/example/wsd_crawling/auth/model/User.java
@@ -0,0 +1,11 @@
+package com.example.wsd_crawling.auth.model;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+
+    // Getter, Setter, Constructor
+}
