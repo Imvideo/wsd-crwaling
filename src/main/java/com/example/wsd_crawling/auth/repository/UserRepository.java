41be68b0aa8cbb276f0a8091983345ac67dc57fb
Jeon Young Sang <iwtl0303@naver.com>
2024-12-07 20:36:51 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/example/wsd_crawling/auth/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.wsd_crawling.auth.repository;
+
+import com.example.wsd_crawling.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
