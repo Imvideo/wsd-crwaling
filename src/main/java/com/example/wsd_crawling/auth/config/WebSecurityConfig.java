ac07f35efa1931cb25df1d1658e53d40e092394f
Jeon Young Sang <iwtl0303@naver.com>
2024-12-07 20:37:34 +0900

    feat: WebSecurityConfig 추가

---
+++ src/main/java/com/example/wsd_crawling/auth/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.wsd_crawling.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/auth/register").permitAll()  // 로그인과 회원 가입은 모두 허용
+                .anyRequest().authenticated();  // 나머지 API는 인증 필요
+    }
+}
