37b7fa49c8449306430cc67957b7b760d61cb2f0
Jeon Young Sang <iwtl0303@naver.com>
2024-12-07 23:44:19 +0900

    fix: UserRepository 수정

--- src/main/java/com/example/wsd_crawling/WsdCrawlingApplication.java
+++ src/main/java/com/example/wsd_crawling/WsdCrawlingApplication.java
@@ -2,12 +2,13 @@ package com.example.wsd_crawling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.wsd_crawling.auth.repository") // 리포지토리 패키지 경로
 public class WsdCrawlingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsdCrawlingApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/wsd_crawling/auth/repository/UserRepository.java
+++ src/main/java/com/example/wsd_crawling/auth/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.wsd_crawling.auth.repository;
 
 import com.example.wsd_crawling.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    // 필요한 추가 메서드 정의
 }
