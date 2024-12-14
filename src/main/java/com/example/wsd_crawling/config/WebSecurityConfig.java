8f010e1bed2af5e2e602f97e650aa3f846b6d9c9
Jeon Young Sang <iwtl0303@naver.com>
2024-12-14 22:01:18 +0900

    feat: 배포 설정 추가

--- src/main/java/com/example/wsd_crawling/config/WebSecurityConfig.java
+++ src/main/java/com/example/wsd_crawling/config/WebSecurityConfig.java
@@ -15,32 +15,32 @@ public class WebSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    // JwtAuthenticationFilter를 생성자 주입
     public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    // PasswordEncoder Bean
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
-    // SecurityFilterChain Bean for HTTP security configuration
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())  // CSRF 비활성화
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers(HttpMethod.GET, "/applications/**","/bookmarks/**").permitAll() // GET 요청 허용
-                                .requestMatchers("/auth/login", "/auth/register","/auth/refresh","/jobs/**","/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/applications/**", "/bookmarks/**").permitAll() // GET 요청 허용
+                                .requestMatchers(
+                                        "/auth/login", "/auth/register", "/auth/refresh",
+                                        "/jobs/**",
+                                        "/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
+                                ).permitAll()
                                 .anyRequest().authenticated()  // 나머지 요청은 인증 필요
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // JWT 인증 필터 추가
 
-        return http.build();  // HttpSecurity 설정 반환
+        return http.build();
     }
-
 }
+
